d93278c513f520053bcaa1b9dabf48eef4eba987
Mileto <milenkotomasevic41@gmail.com>
2024-08-16 23:02:22 -0400

    dev: Added handlesubmit and error message

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,7 +1,8 @@
 import { useCryptoStore } from '../store';
 import { currencies } from '../data';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Pair } from '../types';
+import ErrorMessage from './ErrorMessage';
 
 export default function CryptoSearchForm() {
 	const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
@@ -9,20 +10,40 @@ export default function CryptoSearchForm() {
 		currency: '',
 		cryptocurrency: '',
 	});
+
+	const [error, setError] = useState('');
+
 	const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
 		setPair({
 			...pair,
 			[e.target.name]: e.target.value,
-		})
+		});
 	};
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if(Object.values(pair).includes('')){
+			setError('Todos los campos son obligatorios');
+			return;
+		}
+		setError('');
+		// Consulta a la API
+
+	};
+
 	return (
-		<form className='form'>
+		<form
+			className='form'
+			onSubmit={handleSubmit}
+		>
+			{error && <ErrorMessage>{error}</ErrorMessage>}
 			<div className='field'>
 				<label htmlFor='currency'>Moneda:</label>
 				<select
 					name='currency'
 					id='currency'
 					onChange={handleChange}
+					value={pair.currency}
 				>
 					<option value=''>-- Seleccione --</option>
 					{currencies.map((currency) => (
@@ -41,6 +62,7 @@ export default function CryptoSearchForm() {
 					name='cryptocurrency'
 					id='cryptocurrency'
 					onChange={handleChange}
+					value={pair.cryptocurrency}
 				>
 					<option value=''>-- Seleccione --</option>
 					{cryptocurrencies.map((crypto) => (
