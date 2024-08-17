963617cda6f0f13eb124b259e43fa36569f298f1
Mileto <milenkotomasevic41@gmail.com>
2024-08-16 23:26:04 -0400

    dev: Added cryproprice

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { CryptoCurrency, Pair } from './types';
+import { CryptoCurrency, CryptoPrice, Pair } from './types';
 import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoService';
 
 type CryptoStore = {
 	cryptocurrencies: CryptoCurrency[];
+	result: CryptoPrice;
 	fetchCryptos: () => Promise<void>;
 	fetchData: (pair: Pair) => Promise<void>;
 };
@@ -12,13 +13,14 @@ type CryptoStore = {
 export const useCryptoStore = create<CryptoStore>()(
 	devtools((set) => ({
 		cryptocurrencies: [],
+		result: {} as CryptoPrice,
 		fetchCryptos: async () => {
 			const cryptocurrencies = await getCryptos();
 			set(() => ({ cryptocurrencies }));
 		},
 		fetchData: async (pair) => {
 			const result = await fetchCurrentCryptoPrice(pair);
-			console.log(result);
+			set(() => ({ result }));
 		},
 	}))
 );
