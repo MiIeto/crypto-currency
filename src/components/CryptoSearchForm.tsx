77edbc62f1e6573b8c224bde246f5803a052e66e
Mileto <milenkotomasevic41@gmail.com>
2024-08-16 23:21:44 -0400

    dev: Added fetch price to form

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -6,6 +6,7 @@ import ErrorMessage from './ErrorMessage';
 
 export default function CryptoSearchForm() {
 	const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+	const fetchData = useCryptoStore((state) => state.fetchData);
 	const [pair, setPair] = useState<Pair>({
 		currency: '',
 		cryptocurrency: '',
@@ -28,6 +29,7 @@ export default function CryptoSearchForm() {
 		}
 		setError('');
 		// Consulta a la API
+		fetchData(pair);
 
 	};
 
