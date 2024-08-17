4eea5cb61660d17b8c0f564bd4b5ff42c2fcffe6
Mileto <milenkotomasevic41@gmail.com>
2024-08-16 22:35:45 -0400

    dev: Added store to create currencies

---
+++ src/store.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { create } from 'zustand';
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
+import { CryptoCurrency } from './types';
+
+type CryptoStore = {
+	cryptocurrencies: CryptoCurrency[];
+	fetchCryptos: () => Promise<void>;
+};
+
+async function getCryptos() {
+	const url =
+		'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+	const {
+		data: { Data },
+	} = await axios(url);
+	const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+	if (result.success) {
+		return result.data;
+	}
+}
+
+export const useCryptoStore = create<CryptoStore>((set) => ({
+	cryptocurrencies: [],
+	fetchCryptos: async () => {
+		const cryptocurrencies = await getCryptos();
+		set(() => ({ cryptocurrencies }));
+	},
+}));
