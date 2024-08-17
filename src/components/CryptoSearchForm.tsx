660e841f44a3c8fd8b145bf630174c71e1db1e7c
Mileto <milenkotomasevic41@gmail.com>
2024-08-16 22:12:18 -0400

    dev: Added options of currency to form

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,6 +1,8 @@
+import { currencies } from '../data';
+
 export default function CryptoSearchForm() {
 	return (
-		<form className="form">
+		<form className='form'>
 			<div className='field'>
 				<label htmlFor='currency'>Moneda:</label>
 				<select
@@ -8,6 +10,14 @@ export default function CryptoSearchForm() {
 					id='currency'
 				>
 					<option value=''>-- Seleccione --</option>
+					{currencies.map((currency) => (
+						<option
+							key={currency.code}
+							value={currency.code}
+						>
+							{currency.name}
+						</option>
+					))}
 				</select>
 			</div>
 			<div className='field'>
